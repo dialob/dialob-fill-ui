b61640246e65228f9304b7b62959a091a82243c0
Tomi Pakarinen <tomi.pakarinen@resys.io>
2016-09-02 15:49:01 +0300

    remove redux-thunk support

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,8 +1,6 @@
 import {applyMiddleware} from 'redux';
 import {websocketMiddleware} from './SockJSMiddleware';
-import thunk from 'redux-thunk';
 
 export const middleware = applyMiddleware(
-    thunk,
     websocketMiddleware
 );
