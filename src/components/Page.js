90d5f455227c4ebca59fb5eae48325ca23a8bf8f
Villu Vaimets <villu.vaimets@resys.io>
2016-10-11 16:38:06 +0300

    Complete if all answered and valid

--- src/components/Page.js
+++ src/components/Page.js
@@ -19,6 +19,7 @@ import {connect} from 'react-redux';
 import PageBar from './PageBar';
 import {nextPage, previousPage, completeQuestionnaire} from '../actions/Actions';
 import {getItemList} from '../utils/formUtils';
+import classnames from 'classnames';
 
 require('styles/page.scss');
 
@@ -67,29 +68,39 @@ class Page extends React.Component {
   render() {
     let items = null;
     let page = this.props.page && this.props.page[1];
+    let footer = null;
     if (page) {
       let formItems = this.context.itemList(page);
       let toRender = [];
+      let lastAnswered = false;
       for (let i = 0; i < formItems.length; i++) {
         if (formItems[i].get('type') === 'note') {
           toRender.push(formItems[i]);
+          lastAnswered = true;
           continue;
         }
         let value = formItems[i].get('value');
          if (typeof(value) === 'undefined' || value == null || this.hasErrors(formItems[i])) {
           toRender.push(formItems[i]);
+          lastAnswered = false;
           break;
          }
          if (typeof(value) !== 'undefined' || value != null) {
           toRender.push(formItems[i]);
+          lastAnswered = true;
           continue;
         }
       }
       items = toRender.map(this.context.componentCreator).filter(item => item);
+      footer = (toRender.length === formItems.length && lastAnswered) ?
+      (<span className={classnames('ff-complete-button', 'ff-button-enabled')} onClick={this.completeTouch.bind(this)}>
+        <i className='fa fa-check'></i>
+      </span>) : null;
     }
     return (
       <div className='ff-page'>
         {items}
+        {footer}
       </div>
     );
   }
