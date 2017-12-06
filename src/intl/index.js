c98b66c26c87a059b73f0d461ad855b1409e5df5
Villu Vaimets <villu.vaimets@resys.io>
2017-12-06 22:26:23 +0200

    intl-d boolean question

--- src/intl/index.js
+++ src/intl/index.js
@@ -1,11 +1,17 @@
 module.exports = {
   'en': {
-    "invalidFormat.number": "Invalid format"
+    "invalidFormat.number": "Invalid format",
+    "yes": "Yes",
+    "no": "No"
   },
   'fi': {
-    "invalidFormat.number": "Tarkista määrä"
+    "invalidFormat.number": "Tarkista määrä",
+    "yes": "Kyllä",
+    "no": "Ei"
   },
   'sv': {
-    "invalidFormat.number": "Kontrollera beloppet"
+    "invalidFormat.number": "Kontrollera beloppet",
+    "yes": "Ja",
+    "no": "Nej"
   }
 };
